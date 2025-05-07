126b7599a58de46fbcdc4831907d60da8cc95b97
bojunliu0818 <bliu293@wisc.edu>
2025-05-07 14:37:32 -0500

    fix path issue in CI

--- setup.py
+++ setup.py
@@ -210,7 +210,7 @@ setup(name='msmbuilder2022',
                          ],
       },
       install_requires=[
-          'numpy<=1.26',
+          'numpy<2.1',
           'mdtraj',
           'scikit-learn',
           'pandas',
